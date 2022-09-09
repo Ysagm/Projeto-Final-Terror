5e0233ea1f96e958886ce16b6904340ffacd6556
Ysadora G. M <ysagm@icloud.com>
2022-09-09 15:50:50 -0300

    Mudancas do Photon p1

---
+++ Projeto Final - Horror/Assets/Scripts/GameConnection.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameConnection : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
