d6f17327924b6b5d3ca26e9283224f89d9118080
Carol <carolvargasy@gmail.com>
2022-10-11 22:18:16 -0300

    Blackout Scene

--- Projeto Final - Horror/Assets/Scripts/MenuManager.cs
+++ Projeto Final - Horror/Assets/Scripts/MenuManager.cs
@@ -8,6 +8,7 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private string nameOfLevel;
     [SerializeField] private GameObject Menu;
     [SerializeField] private GameObject Intro;
+    [SerializeField] private GameObject Blackout;
     [SerializeField] private GameObject Options;
     [SerializeField] private GameObject Extra;
     [SerializeField] private GameObject Credits;
@@ -15,8 +16,7 @@ public class MenuManager : MonoBehaviour
 
     public void Play(){
         SceneManager.LoadScene(nameOfLevel);
-
-    }
+    } 
     public void OpenIntro(){
         Menu.SetActive(false);
         Intro.SetActive(true);
@@ -26,6 +26,34 @@ public class MenuManager : MonoBehaviour
         Intro.SetActive(false);
         Menu.SetActive(true);
     }
+    public void OpenBlackout(){
+        Menu.SetActive(false);
+        Blackout.SetActive(true);
+        Debug.Log("Abriu blackout");
+   	}
+    public void CloseBlackout(){
+        Blackout.SetActive(false);
+        Menu.SetActive(true);
+    }
+
+    //TENTATIVA EXTRA PARA SEGURAR ALGUNS SEGUNDOS NA TELA DE BLACKOUT
+    /*
+    void Start(){
+    // set color of the panel transparent
+    GameObject.Find("Canvas/Blackout").GetComponent<Image>().color = new Color(0,0,0,0);
+ 
+    //call GoBlack function after random 1-5 seconds 
+    Invoke("GoBlack",Random.Range(1,5));
+    }
+ 
+    void GoBlack()
+    {
+    // set color of the panel - black
+    GameObject.Find("Canvas/Blackout").GetComponent<Image>().color = new Color(0,0,0,255);
+    }
+    */
+
+
     public void OpenOptions(){
         Menu.SetActive(false);
         Options.SetActive(true);
